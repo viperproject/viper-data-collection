bf05d92baaad536f643ada6d81d085dfc5ef74df
Simon Hostettler <simon.hostettler@protonmail.com>
2023-10-26 15:03:43 +0200

    added carbon results

--- src/main/scala/dataCollection/SimilarityChecker.scala
+++ src/main/scala/dataCollection/SimilarityChecker.scala
@@ -1,78 +1,80 @@
 package dataCollection
 
 import upickle.default.{macroRW, read, write, ReadWriter => RW}
-import viper.silicon.SiliconRunner.runMain
-import viper.silicon.{Config, SiliconFrontend, SiliconRunner, SiliconRunnerInstance, reporting}
-import viper.silver.ast.{NoPosition, Position}
-import viper.silver.frontend.DefaultStates
-import viper.silver.parser.{FastParser, PProgram}
+import viper.silicon.{Config, SiliconFrontend, reporting}
+import viper.carbon.CarbonFrontend
+import viper.silver.logger.ViperStdOutLogger
+import viper.silver.parser.FastParser
 import viper.silver.reporter.{ExceptionReport, InternalWarningMessage, StdIOReporter}
-import viper.silver.verifier.{AbstractError, Success, VerificationResult}
-import viper.silver.verifier.{AbstractVerificationError => SilAbstractVerificationError, Failure => SilFailure, Success => SilSuccess, TimeoutOccurred => SilTimeoutOccurred, VerificationResult => SilVerificationResult, Verifier => SilVerifier}
+import viper.silver.verifier.{AbstractError, VerificationResult}
+import viper.silver.verifier.{Failure => SilFailure, Success => SilSuccess}
 
-import java.io.{BufferedWriter, File, FileWriter}
-import java.nio.file.{Files, Path, Paths}
+import java.io.{BufferedWriter, FileWriter}
+import java.nio.charset.CodingErrorAction
+import java.nio.file.{Path, Paths}
 import scala.collection.immutable.ArraySeq
-import scala.io.{BufferedSource, Source}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.io.{BufferedSource, Codec}
 import scala.io.Source.fromFile
-import scala.math.Ordered.orderingToOrdered
+import scala.language.postfixOps
 
 object SimTestRunner extends App {
-  private val testFolder = "/Users/simon/code/silicon/src/test/resources/dataCollection/results/"
+  private val testFolder = "/Users/simon/code/viper-data-collection/src/test/resources/dataCollection/results/"
   val starttime = System.currentTimeMillis()
   var progresults: Seq[ProgAnalysisResult] = Seq()
-  for (num <- Seq.range(100,479)) {
+  for (num <- Seq.range(0, 901)) {
     val sourcefile: BufferedSource = fromFile(testFolder + s"prog${num}_analysis.json")
     val pprintJSON: String = try sourcefile.mkString finally sourcefile.close()
     val progres = read(pprintJSON)(ProgAnalysisResult.rw)
     progresults = progresults :+ progres
   }
   var dupCount = 0
-  var errCount = 0
-  for (num <- Seq.range(100, 479)) {
-    val prog1 = progresults(num-100)
-    if(!(prog1.silRes.verRes == Some(VerRes(true, List())))) errCount +=1
+  for (num <- Seq.range(0, 901)) {
+    val prog1 = progresults(num)
     var matches: Seq[Int] = Seq()
-    for(num2 <- Seq.range(num + 1, 479)) {
-      val prog2 = progresults(num2-100)
+    for (num2 <- Seq.range(num + 1, 901)) {
+      val prog2 = progresults(num2)
       if (prog1 != prog2) {
-        if(prog1.isSimilarTo(prog2) && prog2.isSimilarTo(prog1)) {
+        if (prog1.isSimilarTo(prog2) && prog2.isSimilarTo(prog1)) {
           matches = matches :+ num2
-          //println(s"MATCH FOUND: ${prog1.progName} and ${prog2.progName}")
-          //println(prog1.pprint.matchTrees(prog2.pprint))
         }
       }
     }
     println(s"Matches with ${num}: ${matches}")
-    if (!(matches == List())) dupCount +=1
+    if (!(matches == List())) dupCount += 1
   }
   println(s"${dupCount} duplicates found")
-  println(s"${errCount} errors found")
   println(s"Time: ${System.currentTimeMillis() - starttime}ms")
 }
 
-/** Represents the result of the [[ProgramAnalyser]] for later comparison*/
-case class ProgAnalysisResult(progName: String, silRes: SiliconResult, pprint: ProgramPrint, flags: Seq[String]) {
+/** Represents the result of the [[ProgramAnalyser]] for later comparison */
+case class ProgAnalysisResult(progName: String, siliconRes: VerifierResult, carbonRes: VerifierResult, pprint: ProgramPrint, flags: Seq[String]) {
   def isSimilarTo(other: ProgAnalysisResult): Boolean = {
     lazy val sameFlags = this.flags.toSet == other.flags.toSet
-    lazy val similarResult = this.silRes.isSimilarTo(other.silRes, 1.5)
+    lazy val similarSilResult = this.siliconRes.isSimilarTo(other.siliconRes, 1.5)
+    lazy val similarCarbonResult = this.carbonRes.isSimilarTo(other.carbonRes, 1.5)
     lazy val matchResult = this.pprint.matchTrees(other.pprint)
     lazy val similarMethods = matchResult.methodMatchPercentage >= 80
     lazy val similarPreamble = matchResult.preambleMatchPercentage >= 80
 
-    similarResult && sameFlags && similarPreamble && similarMethods
+    sameFlags && similarSilResult && similarCarbonResult && similarPreamble && similarMethods
   }
 }
+
 object ProgAnalysisResult {
   implicit val rw: RW[ProgAnalysisResult] = macroRW
 }
 
-/** Result of running a program through the Silicon Verifier
+/** Result of running a program through some Viper Verifier
+ *
  * @param verRes is the verification result, including any errors encountered
- * @param time is the runtime it took for the verifier to finish
+ * @param time   is the runtime it took for the verifier to finish
  */
-case class SiliconResult(verRes: Option[VerRes], time: Long) {
-  def isSimilarTo(other: SiliconResult, timeEps: Double): Boolean = {
+case class VerifierResult(verRes: Option[VerRes], time: Long) {
+  def isSimilarTo(other: VerifierResult, timeEps: Double): Boolean = {
     lazy val sameRes: Boolean = this.verRes match {
       case Some(res) => other.verRes match {
         case Some(otherRes) => res.isSimilarTo(otherRes)
@@ -84,11 +86,12 @@ case class SiliconResult(verRes: Option[VerRes], time: Long) {
     similarTime && sameRes
   }
 }
-object SiliconResult {
-  implicit val rw: RW[SiliconResult] = macroRW
+
+object VerifierResult {
+  implicit val rw: RW[VerifierResult] = macroRW
 }
 
-/** A wrapper class for a [[VerificationResult]] to facilitate comparison and serialization*/
+/** A wrapper class for a [[VerificationResult]] to facilitate comparison and serialization */
 case class VerRes(success: Boolean, errors: Seq[VerError]) {
   def isSimilarTo(other: VerRes): Boolean = {
     success match {
@@ -97,8 +100,10 @@ case class VerRes(success: Boolean, errors: Seq[VerError]) {
     }
   }
 }
+
 object VerRes {
   implicit val rw: RW[VerRes] = macroRW
+
   def toVerRes(vr: VerificationResult): VerRes = vr match {
     case SilSuccess => VerRes(true, Seq.empty)
     case SilFailure(errors) => VerRes(false, errors map VerError.toError)
@@ -106,16 +111,19 @@ object VerRes {
 
 }
 
-/** A wrapper class for an [[AbstractError]] to facilitate comparison and serialization and remove unneeded information*/
+/** A wrapper class for an [[AbstractError]] to facilitate comparison and serialization and remove unneeded information */
 case class VerError(id: String, message: String) {
   override def equals(obj: Any): Boolean = obj match {
     case that: VerError => this.id == that.id
     case _ => false
   }
+
   override def hashCode(): Int = id.hashCode
 }
+
 object VerError {
   implicit val rw: RW[VerError] = macroRW
+
   def toError(ae: AbstractError): VerError = {
     VerError(ae.fullId, ae.readableMessage)
   }
@@ -125,33 +133,44 @@ object SimilarityChecker {
 }
 
 /** Object to analyse programs for later similarity comparison
+ *
  * @*/
 object ProgramAnalyser {
   private val fastParser = new FastParser()
+  val decoder = Codec.UTF8.decoder.onMalformedInput(CodingErrorAction.IGNORE)
 
   /** Analyses a program and returns the results of the verifier and a fingerprint of its AST */
   def runProgAnalysis(args: Array[String]): ProgAnalysisResult = {
     val fileName = Paths.get(args(0)).getFileName.toString.split("\\.").head
-    val silRes = getVerificationResults(args)
+    val silRes = getSilVerifierResults(args)
+    val carbonRes = getCarbonVerifierResults(args)
     val pprint = getProgramPrint(Paths.get(args(0)))
-    ProgAnalysisResult(fileName, silRes, pprint, args.tail)
+    ProgAnalysisResult(fileName, silRes, carbonRes, pprint, args.tail)
   }
 
-  private def getVerificationResults(args: Array[String]): SiliconResult = {
-    val runner = new SimSilRunnerInstance
+  private def getSilVerifierResults(args: Array[String]): VerifierResult = {
+    val runner = new SiliconInstance
     runner.runMain(args)
-    SiliconResult(runner.getVerificationResult.map(v => VerRes.toVerRes(v)), runner.getTime)
+    VerifierResult(runner.getVerificationResult.map(v => VerRes.toVerRes(v)), runner.getTime)
+  }
+
+  private def getCarbonVerifierResults(args: Array[String]): VerifierResult = {
+    val runner = new CarbonInstance
+    runner.main(args)
+    VerifierResult(runner.getVerificationResult.map(v => VerRes.toVerRes(v)), runner.getTime)
   }
 
   private def getProgramPrint(p: Path): ProgramPrint = {
-    val argString = Source.fromInputStream(Files.newInputStream(p)).mkString
+    val sourcefile: BufferedSource = fromFile(p.toString)(decoder)
+    val argString: String = try sourcefile.mkString finally sourcefile.close()
     val parsedProgram = fastParser.parse(argString, p)
     Fingerprinter.fingerprintPProgram(parsedProgram)
   }
 }
 
 object AnalysisRunner {
-  val testFolder = "/Users/simon/code/silicon/src/test/resources/dataCollection/results/"
+  val testFolder = "/Users/simon/code/viper-data-collection/src/test/resources/dataCollection/results/"
+
   def main(args: Array[String]): Unit = {
     val fileName = Paths.get(args(0)).getFileName.toString.split("\\.").head
     val programData = ProgramAnalyser.runProgAnalysis(args)
@@ -162,9 +181,16 @@ object AnalysisRunner {
   }
 }
 
+//TODO: Somehow manage timeouts
+class CarbonInstance extends CarbonFrontend(StdIOReporter("carbon_reporter"), ViperStdOutLogger("Carbon", "INFO").get) {
+  def main(args: Array[String]): Unit = {
+    execute(Seq(args(0)))
+  }
+}
 
-/** Silicon frontend implementation that doesn't exit the program once verification is done*/
-class SimSilRunnerInstance extends SiliconFrontend(StdIOReporter()) {
+
+/** Silicon frontend implementation that doesn't exit the program once verification is done */
+class SiliconInstance extends SiliconFrontend(StdIOReporter()) {
   def runMain(args: Array[String]): Unit = {
 
     try {
