73897b3280480e41677244a40fdc43a315c347e1
Simon Hostettler <simon.hostettler@protonmail.com>
2023-11-03 14:53:58 +0100

    added serialization for db tables

--- src/main/scala/dataCollection/Models.scala
+++ src/main/scala/dataCollection/Models.scala
@@ -1,25 +1,78 @@
 package dataCollection
 
+import upickle.default.{macroRW, read, write, ReadWriter => RW}
 import viper.silver.parser.{Nodes, PExists, PForPerm, PForall, PNode, PProgram, PQuantifier}
 import slick.jdbc.MySQLProfile.api._
 import slick.lifted.ProvenShape
+
 case class ProgramEntry(id: Long,
                         originalName: String,
                         program: String,
                         loc: Int,
                         frontend: String,
                         originalVerifier: String,
-                        siliconResJSON: String,
-                        carbonResJSON: String,
-                        argsJSON: String,
-                        siliconPhaseRuntimesJSON: String,
-                        carbonPhaseRuntimesJSON: String,
-                        programPrintJSON: String,
+                        siliconRes: Option[VerifierResult],
+                        carbonRes: Option[VerifierResult],
+                        args: Seq[String],
+                        siliconPhaseRuntimes: Seq[(String, Long)],
+                        carbonPhaseRuntimes: Seq[(String, Long)],
+                        programPrint: ProgramPrint,
                         parseSuccess: Boolean,
                         hasPreamble: Boolean)
 
+object ProgramEntry {
+  def serialize(pE: ProgramEntry): SerializedProgramEntry = {
+    SerializedProgramEntry(pE.id,
+      pE.originalName,
+      pE.program,
+      pE.loc,
+      pE.frontend,
+      pE.originalVerifier,
+      write(pE.siliconRes),
+      write(pE.carbonRes),
+      write(pE.args),
+      write(pE.siliconPhaseRuntimes),
+      write(pE.carbonPhaseRuntimes),
+      write(pE.programPrint),
+      pE.parseSuccess,
+      pE.hasPreamble)
+  }
 
-class ProgramEntryTable(tag: Tag) extends Table[ProgramEntry](tag, Some("programs"), "ProgramEntries") {
+  def deserialize(pE: SerializedProgramEntry): ProgramEntry = {
+    ProgramEntry(pE.id,
+      pE.originalName,
+      pE.program,
+      pE.loc,
+      pE.frontend,
+      pE.originalVerifier,
+      read(pE.siliconResJSON),
+      read(pE.carbonResJSON),
+      read(pE.argsJSON),
+      read(pE.siliconPhaseRuntimesJSON),
+      read(pE.carbonPhaseRuntimesJSON),
+      read(pE.programPrintJSON),
+      pE.parseSuccess,
+      pE.hasPreamble)
+  }
+}
+
+case class SerializedProgramEntry(id: Long,
+                                  originalName: String,
+                                  program: String,
+                                  loc: Int,
+                                  frontend: String,
+                                  originalVerifier: String,
+                                  siliconResJSON: String,
+                                  carbonResJSON: String,
+                                  argsJSON: String,
+                                  siliconPhaseRuntimesJSON: String,
+                                  carbonPhaseRuntimesJSON: String,
+                                  programPrintJSON: String,
+                                  parseSuccess: Boolean,
+                                  hasPreamble: Boolean)
+
+
+class ProgramEntryTable(tag: Tag) extends Table[SerializedProgramEntry](tag, Some("programs"), "ProgramEntries") {
   private def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
 
   private def originalName = column[String]("originalName")
@@ -36,7 +89,7 @@ class ProgramEntryTable(tag: Tag) extends Table[ProgramEntry](tag, Some("program
 
   private def carbonResJSON = column[String]("carbonResJSON")
 
-  private def argsJSON = column[String]("flags")
+  private def argsJSON = column[String]("argsJSON")
 
   private def siliconPhaseRuntimesJSON = column[String]("siliconPhaseRuntimesJSON")
 
@@ -48,7 +101,7 @@ class ProgramEntryTable(tag: Tag) extends Table[ProgramEntry](tag, Some("program
 
   private def hasPreamble = column[Boolean]("hasPreamble")
 
-  override def * : ProvenShape[ProgramEntry] = (id,
+  override def * : ProvenShape[SerializedProgramEntry] = (id,
     originalName,
     program,
     loc,
@@ -61,7 +114,7 @@ class ProgramEntryTable(tag: Tag) extends Table[ProgramEntry](tag, Some("program
     carbonPhaseRuntimesJSON,
     programPrintJSON,
     parseSuccess,
-    hasPreamble) <> (ProgramEntry.tupled, ProgramEntry.unapply)
+    hasPreamble) <> (SerializedProgramEntry.tupled, SerializedProgramEntry.unapply)
 
 }
 
@@ -70,11 +123,44 @@ case class UserSubmission(id: Long,
                           program: String,
                           loc: Int,
                           frontend: String,
-                          argsJSON: String,
+                          args: Seq[String],
                           originalVerifier: String,
                           success: Boolean)
 
-class UserSubmissionTable(tag: Tag) extends Table[UserSubmission](tag, Some("programs"), "UserSubmissions") {
+object UserSubmission {
+  def serialize(uS: UserSubmission): SerializedUserSubmission = {
+    SerializedUserSubmission(uS.id,
+      uS.originalName,
+      uS.program,
+      uS.loc,
+      uS.frontend,
+      write(uS.args),
+      uS.originalVerifier,
+      uS.success)
+  }
+
+  def deserialize(uS: SerializedUserSubmission): UserSubmission = {
+    UserSubmission(uS.id,
+      uS.originalName,
+      uS.program,
+      uS.loc,
+      uS.frontend,
+      read(uS.argsJSON),
+      uS.originalVerifier,
+      uS.success)
+  }
+}
+
+case class SerializedUserSubmission(id: Long,
+                                    originalName: String,
+                                    program: String,
+                                    loc: Int,
+                                    frontend: String,
+                                    argsJSON: String,
+                                    originalVerifier: String,
+                                    success: Boolean)
+
+class UserSubmissionTable(tag: Tag) extends Table[SerializedUserSubmission](tag, Some("programs"), "UserSubmissions") {
   private def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
 
   private def originalName = column[String]("originalName")
@@ -85,20 +171,20 @@ class UserSubmissionTable(tag: Tag) extends Table[UserSubmission](tag, Some("pro
 
   private def frontend = column[String]("frontend")
 
-  private def argsJSON = column[String]("flags")
+  private def argsJSON = column[String]("argsJSON")
 
   private def originalVerifier = column[String]("originalVerifier")
 
   private def success = column[Boolean]("success")
 
-  override def * : ProvenShape[UserSubmission] = (id,
+  override def * : ProvenShape[SerializedUserSubmission] = (id,
     originalName,
     program,
     loc,
     frontend,
     argsJSON,
     originalVerifier,
-    success) <> (UserSubmission.tupled, UserSubmission.unapply)
+    success) <> (SerializedUserSubmission.tupled, SerializedUserSubmission.unapply)
 
 }
 
